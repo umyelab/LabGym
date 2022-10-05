540c5f0372cf82de2112b3870926ba47577bbc25
YUJIA HU <henry@YUJIAs-MacBook-Pro.local>
2022-10-05 01:57:18 -0400

    update setup.py to v1.5

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ except Exception:
     long_description=''
 
 
-setup(name='LabGym',version='1.4.1',author='Yujia Hu',
+setup(name='LabGym',version='1.5',author='Yujia Hu',
     author_email='henryhu@umich.edu',
     description='Quantification of user-defined animal behaviors',
     long_description=long_description,
