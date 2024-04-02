495e610e2ddff834287dafcb6f88e615d48c094b
Yujia Hu <yujiahu415@gmail.com>
2024-04-02 13:41:39 -0400

    fix version checker

--- LabGym/__main__.py
+++ LabGym/__main__.py
@@ -1,8 +1,26 @@
-import json
-from pathlib import Path
-from urllib import request
-from urllib.error import URLError
+'''
+Copyright (C)
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see https://tldrlegal.com/license/gnu-general-public-license-v3-(gpl-3)#fulltext. 
 
+For license issues, please contact:
+
+Dr. Bing Ye
+Life Sciences Institute
+University of Michigan
+210 Washtenaw Avenue, Room 5403
+Ann Arbor, MI 48109-2216
+USA
+
+Email: bingye@umich.edu
+'''
+
+
+
+
+import requests
+from pathlib import Path
 from packaging import version
 
 from LabGym import __version__, gui
@@ -13,7 +31,7 @@ def main() -> None:
 
     try:
         current_version = version.parse(__version__)
-        pypi_json = json.loads(request.urlopen("https://pypi.python.org/pypi/LabGym/json").read())
+        pypi_json = requests.get("https://pypi.org/pypi/LabGym/json").json()
         latest_version = version.parse(pypi_json["info"]["version"])
 
         if latest_version > current_version:
@@ -25,7 +43,7 @@ def main() -> None:
             print(f"You are using LabGym {current_version}, but version {latest_version} is available.")
             print(f"Consider upgrading LabGym by using the command '{upgrade_command}'.")
             print("For the details of new changes, check https://github.com/umyelab/LabGym.\n")
-    except URLError:
+    except:
         print("Unable to check for new versions of LabGym!")
         print("Please check https://github.com/umyelab/LabGym for updates.\n")
 
