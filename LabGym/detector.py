b0518d672e2daa2009ec449ab3bb8027f224432c
Yujia Hu <yujiahu415@gmail.com>
2024-07-14 11:54:01 -0400

    Update detector.py

--- LabGym/detector.py
+++ LabGym/detector.py
@@ -23,14 +23,18 @@ import os
 import cv2
 import json
 import torch
-from detectron2 import model_zoo
-from detectron2.checkpoint import DetectionCheckpointer
-from detectron2.config import get_cfg
-from detectron2.data import MetadataCatalog,DatasetCatalog,build_detection_test_loader
-from detectron2.data.datasets import register_coco_instances
-from detectron2.engine import DefaultTrainer,DefaultPredictor
-from detectron2.utils.visualizer import Visualizer
-from detectron2.evaluation import COCOEvaluator,inference_on_dataset
+try:
+	from detectron2 import model_zoo
+	from detectron2.checkpoint import DetectionCheckpointer
+	from detectron2.config import get_cfg
+	from detectron2.data import MetadataCatalog,DatasetCatalog,build_detection_test_loader
+	from detectron2.data.datasets import register_coco_instances
+	from detectron2.engine import DefaultTrainer,DefaultPredictor
+	from detectron2.utils.visualizer import Visualizer
+	from detectron2.evaluation import COCOEvaluator,inference_on_dataset
+except:
+	print('You need to install Detectron2 to use the Detector module in LabGym:')
+	print('https://detectron2.readthedocs.io/en/latest/tutorials/install.html')
 
 
 
@@ -41,32 +45,32 @@ def traindetector(path_to_annotation,path_to_trainingimages,path_to_detector,ite
 	else:
 		device='cpu'
 
-	if str('FluoSA_detector_train') in DatasetCatalog.list():
-		DatasetCatalog.remove('FluoSA_detector_train')
-		MetadataCatalog.remove('FluoSA_detector_train')
+	if str('LabGym_detector_train') in DatasetCatalog.list():
+		DatasetCatalog.remove('LabGym_detector_train')
+		MetadataCatalog.remove('LabGym_detector_train')
 
-	register_coco_instances('FluoSA_detector_train',{},path_to_annotation,path_to_trainingimages)
+	register_coco_instances('LabGym_detector_train',{},path_to_annotation,path_to_trainingimages)
 
-	datasetcat=DatasetCatalog.get('FluoSA_detector_train')
-	metadatacat=MetadataCatalog.get('FluoSA_detector_train')
+	datasetcat=DatasetCatalog.get('LabGym_detector_train')
+	metadatacat=MetadataCatalog.get('LabGym_detector_train')
 
 	classnames=metadatacat.thing_classes
 
 	model_parameters_dict={}
-	model_parameters_dict['neuro_names']=[]
+	model_parameters_dict['animal_names']=[]
 
 	annotation_data=json.load(open(path_to_annotation))
 
 	for i in annotation_data['categories']:
 		if i['id']>0:
-			model_parameters_dict['neuro_names'].append(i['name'])
+			model_parameters_dict['animal_names'].append(i['name'])
 
-	print('Neural structure names in annotation file: '+str(model_parameters_dict['neuro_names']))
+	print('Animal names in annotation file: '+str(model_parameters_dict['animal_names']))
 
 	cfg=get_cfg()
 	cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 	cfg.OUTPUT_DIR=path_to_detector
-	cfg.DATASETS.TRAIN=('FluoSA_detector_train',)
+	cfg.DATASETS.TRAIN=('LabGym_detector_train',)
 	cfg.DATASETS.TEST=()
 	cfg.DATALOADER.NUM_WORKERS=4
 	cfg.MODEL.WEIGHTS=model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
@@ -92,11 +96,11 @@ def traindetector(path_to_annotation,path_to_trainingimages,path_to_detector,ite
 
 	model_parameters=os.path.join(cfg.OUTPUT_DIR,'model_parameters.txt')
 	
-	model_parameters_dict['neuro_mapping']={}
+	model_parameters_dict['animal_mapping']={}
 	model_parameters_dict['inferencing_framesize']=int(inference_size)
 
 	for i in range(len(classnames)):
-		model_parameters_dict['neuro_mapping'][i]=classnames[i]
+		model_parameters_dict['animal_mapping'][i]=classnames[i]
 
 	with open(model_parameters,'w') as f:
 		f.write(json.dumps(model_parameters_dict))
@@ -118,24 +122,24 @@ def traindetector(path_to_annotation,path_to_trainingimages,path_to_detector,ite
 
 def testdetector(path_to_annotation,path_to_testingimages,path_to_detector,output_path):
 
-	if str('FluoSA_detector_test') in DatasetCatalog.list():
-		DatasetCatalog.remove('FluoSA_detector_test')
-		MetadataCatalog.remove('FluoSA_detector_test')
+	if str('LabGym_detector_test') in DatasetCatalog.list():
+		DatasetCatalog.remove('LabGym_detector_test')
+		MetadataCatalog.remove('LabGym_detector_test')
 
-	register_coco_instances('FluoSA_detector_test',{},path_to_annotation,path_to_testingimages)
+	register_coco_instances('LabGym_detector_test',{},path_to_annotation,path_to_testingimages)
 
-	datasetcat=DatasetCatalog.get('FluoSA_detector_test')
-	metadatacat=MetadataCatalog.get('FluoSA_detector_test')
+	datasetcat=DatasetCatalog.get('LabGym_detector_test')
+	metadatacat=MetadataCatalog.get('LabGym_detector_test')
 
 	animalmapping=os.path.join(path_to_detector,'model_parameters.txt')
 
 	with open(animalmapping) as f:
 		model_parameters=f.read()
 
-	animal_names=json.loads(model_parameters)['neuro_names']
+	animal_names=json.loads(model_parameters)['animal_names']
 	dt_infersize=int(json.loads(model_parameters)['inferencing_framesize'])
 
-	print('The total categories of neural structures in this Detector: '+str(animal_names))
+	print('The total categories of animals in this Detector: '+str(animal_names))
 	print('The inferencing framesize of this Detector: '+str(dt_infersize))
 
 	cfg=get_cfg()
@@ -148,12 +152,12 @@ def testdetector(path_to_annotation,path_to_testingimages,path_to_detector,outpu
 	for d in datasetcat:
 		im=cv2.imread(d['file_name'])
 		outputs=predictor(im)
-		v=Visualizer(im[:,:,::-1],MetadataCatalog.get('FluoSA_detector_test'),scale=1.2)
+		v=Visualizer(im[:,:,::-1],MetadataCatalog.get('LabGym_detector_test'),scale=1.2)
 		out=v.draw_instance_predictions(outputs['instances'].to('cpu'))
 		cv2.imwrite(os.path.join(output_path,os.path.basename(d['file_name'])),out.get_image()[:,:,::-1])
 
-	evaluator=COCOEvaluator('FluoSA_detector_test',cfg,False,output_dir=output_path)
-	val_loader=build_detection_test_loader(cfg,'FluoSA_detector_test')
+	evaluator=COCOEvaluator('LabGym_detector_test',cfg,False,output_dir=output_path)
+	val_loader=build_detection_test_loader(cfg,'LabGym_detector_test')
 
 	inference_on_dataset(predictor.model,val_loader,evaluator)
 
