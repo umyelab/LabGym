30e7154d0e848b0410446e068390a64745a7c1df
Yujia Hu <yujiahu415@gmail.com>
2024-10-24 14:52:58 -0400

    Update analyzebehavior.py

--- LabGym/analyzebehavior.py
+++ LabGym/analyzebehavior.py
@@ -706,8 +706,8 @@ class AnalyzeAnimal():
 								cy=self.animal_centers[i][frame_count_analyze][1]
 
 								if self.animal_centers[i][max(frame_count_analyze-1,0)] is not None:
-									cxp=self.animal_centers[i][frame_count_analyze-1][0]
-									cyp=self.animal_centers[i][frame_count_analyze-1][1]
+									cxp=self.animal_centers[i][max(frame_count_analyze-1,0)][0]
+									cyp=self.animal_centers[i][max(frame_count_analyze-1,0)][1]
 									cv2.line(self.background,(cx,cy),(cxp,cyp),(abs(int(color_diff*(total_animal_number-current_animal_number)-255)),int(color_diff*current_animal_number/2),int(color_diff*(total_animal_number-current_animal_number)/2)),int(text_tk))
 								else:
 									cv2.circle(self.background,(cx,cy),int(text_tk),(abs(int(color_diff*(total_animal_number-current_animal_number)-255)),int(color_diff*current_animal_number/2),int(color_diff*(total_animal_number-current_animal_number)/2)),-1)
