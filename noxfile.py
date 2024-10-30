d88f39c77c37cd1efacce5217741cf7a7ba55160
Yujia Hu <yujiahu415@gmail.com>
2024-10-30 11:28:38 -0400

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,4 @@
-"""
+'''
 Copyright (C)
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -14,24 +14,22 @@ Ann Arbor, MI 48109-2216
 USA
 
 Email: bingye@umich.edu
-"""
+'''
 
 import nox
 
-nox.options.error_on_missing_interpreters = True
+nox.options.error_on_missing_interpreters=True
 
 
-@nox.session(python=["3.9", "3.10"], reuse_venv=True)
-def tests(session: nox.Session) -> None:
-    """Run the test suite."""
-    session.install("-e", ".")
-    session.install("pytest")
-    session.run("pytest")
+@nox.session(python=['3.9','3.10'],reuse_venv=True)
+def tests(session:nox.Session):
+    session.install('-e','.')
+    session.install('pytest')
+    session.run('pytest')
 
 
 @nox.session(reuse_venv=True)
-def docs(session: nox.Session) -> None:
-    """Build the docs using the sphinx-autobuild server."""
-    session.install("-r", "docs/requirements.txt")
-    session.run("make", "-C", "docs", "clean", external=True)
-    session.run("sphinx-autobuild", "docs", "docs/_build/html")
+def docs(session:nox.Session):
+    session.install('-r','docs/requirements.txt')
+    session.run('make','-C','docs','clean',external=True)
+    session.run('sphinx-autobuild','docs','docs/_build/html')
