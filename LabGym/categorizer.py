c2dacf490034c90b04a7bf74eeb9f0ef24a816e5
Yujia Hu <yujiahu415@gmail.com>
2024-12-06 15:15:12 -0500

    Update categorizer.py

--- LabGym/categorizer.py
+++ LabGym/categorizer.py
@@ -44,6 +44,7 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import pandas as pd
+import itertools
 
 
 
