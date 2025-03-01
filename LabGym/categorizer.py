f9ea1a367830707ef1dbafd97a9da31c5cced4dd
Yujia Hu <yujiahu415@gmail.com>
2025-03-01 12:38:17 -0500

    Update categorizer.py

--- LabGym/categorizer.py
+++ LabGym/categorizer.py
@@ -77,6 +77,8 @@ class DatasetFromPath_AA(Sequence):
 				if classname not in classnames:
 					classnames.append(classname)
 
+		np.random.shuffle(pattern_image_paths)
+
 		classnames.sort()
 		labels=np.array(classnames)
 		lb=LabelBinarizer()
@@ -155,6 +157,8 @@ class DatasetFromPath(Sequence):
 				classname=pattern_image.split('.jpg')[0].split('_')[-1]
 				if classname not in classnames:
 					classnames.append(classname)
+
+		np.random.shuffle(pattern_image_paths)
 
 		classnames.sort()
 		labels=np.array(classnames)
