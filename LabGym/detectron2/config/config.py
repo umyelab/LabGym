e5b748f51897af948fcb1c3d5a38c0e42b1924cf
Yujia Hu <yujiahu415@gmail.com>
2025-06-20 23:25:44 -0400

    Update config.py

--- LabGym/detectron2/config/config.py
+++ LabGym/detectron2/config/config.py
@@ -6,7 +6,7 @@ import inspect
 import logging
 from fvcore.common.config import CfgNode as _CfgNode
 
-from detectron2.utils.file_io import PathManager
+from LabGym.detectron2.utils.file_io import PathManager
 
 
 class CfgNode(_CfgNode):
