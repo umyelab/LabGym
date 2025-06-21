ebe25ed66c2f6af65ec4fa2148a012eb9830ef4d
Yujia Hu <yujiahu415@gmail.com>
2025-06-20 23:39:15 -0400

    update benchmark.py and build.py

--- LabGym/detectron2/data/build.py
+++ LabGym/detectron2/data/build.py
@@ -11,12 +11,12 @@ import torch.utils.data as torchdata
 from tabulate import tabulate
 from termcolor import colored
 
-from detectron2.config import configurable
-from detectron2.structures import BoxMode
-from detectron2.utils.comm import get_world_size
-from detectron2.utils.env import seed_all_rng
-from detectron2.utils.file_io import PathManager
-from detectron2.utils.logger import _log_api_usage, log_first_n
+from LabGym.detectron2.config import configurable
+from LabGym.detectron2.structures import BoxMode
+from LabGym.detectron2.utils.comm import get_world_size
+from LabGym.detectron2.utils.env import seed_all_rng
+from LabGym.detectron2.utils.file_io import PathManager
+from LabGym.detectron2.utils.logger import _log_api_usage, log_first_n
 
 from .catalog import DatasetCatalog, MetadataCatalog
 from .common import AspectRatioGroupedDataset, DatasetFromList, MapDataset, ToIterableDataset
