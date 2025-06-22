56c0de091762bc32f93fc11ad7ca6c2cc0455fbc
Yujia Hu <yujiahu415@gmail.com>
2025-06-21 23:34:11 -0400

    Update launch.py

--- LabGym/detectron2/engine/launch.py
+++ LabGym/detectron2/engine/launch.py
@@ -5,7 +5,7 @@ import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
-from detectron2.utils import comm
+from LabGym.detectron2.utils import comm
 
 __all__ = ["DEFAULT_TIMEOUT", "launch"]
 
