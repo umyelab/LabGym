119d28da99b0185289202ae7c350d489cf83849f
Yujia Hu <yujiahu415@gmail.com>
2025-06-21 23:35:15 -0400

    Update coco_evaluation.py

--- LabGym/detectron2/evaluation/coco_evaluation.py
+++ LabGym/detectron2/evaluation/coco_evaluation.py
@@ -15,18 +15,18 @@ from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 from tabulate import tabulate
 
-import detectron2.utils.comm as comm
-from detectron2.config import CfgNode
-from detectron2.data import MetadataCatalog
-from detectron2.data.datasets.coco import convert_to_coco_json
-from detectron2.structures import Boxes, BoxMode, pairwise_iou
-from detectron2.utils.file_io import PathManager
-from detectron2.utils.logger import create_small_table
+import LabGym.detectron2.utils.comm as comm
+from LabGym.detectron2.config import CfgNode
+from LabGym.detectron2.data import MetadataCatalog
+from LabGym.detectron2.data.datasets.coco import convert_to_coco_json
+from LabGym.detectron2.structures import Boxes, BoxMode, pairwise_iou
+from LabGym.detectron2.utils.file_io import PathManager
+from LabGym.detectron2.utils.logger import create_small_table
 
 from .evaluator import DatasetEvaluator
 
 try:
-    from detectron2.evaluation.fast_eval_api import COCOeval_opt
+    from LabGym.detectron2.evaluation.fast_eval_api import COCOeval_opt
 except ImportError:
     COCOeval_opt = COCOeval
 
