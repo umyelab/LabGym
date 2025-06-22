769bb901a2d17d78dd0d5b3b8d36d16b4bc77d2a
Yujia Hu <yujiahu415@gmail.com>
2025-06-21 23:37:06 -0400

    Update sem_seg_evaluation.py

--- LabGym/detectron2/evaluation/sem_seg_evaluation.py
+++ LabGym/detectron2/evaluation/sem_seg_evaluation.py
@@ -10,9 +10,9 @@ import pycocotools.mask as mask_util
 import torch
 from PIL import Image
 
-from detectron2.data import DatasetCatalog, MetadataCatalog
-from detectron2.utils.comm import all_gather, is_main_process, synchronize
-from detectron2.utils.file_io import PathManager
+from LabGym.detectron2.data import DatasetCatalog, MetadataCatalog
+from LabGym.detectron2.utils.comm import all_gather, is_main_process, synchronize
+from LabGym.detectron2.utils.file_io import PathManager
 
 from .evaluator import DatasetEvaluator
 
