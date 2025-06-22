3584beaf9e4edcc4779bcbd58fdc5ec524788bf7
Yujia Hu <yujiahu415@gmail.com>
2025-06-21 23:36:30 -0400

    Update panoptic_evaluation.py

--- LabGym/detectron2/evaluation/panoptic_evaluation.py
+++ LabGym/detectron2/evaluation/panoptic_evaluation.py
@@ -12,9 +12,9 @@ from typing import Optional
 from PIL import Image
 from tabulate import tabulate
 
-from detectron2.data import MetadataCatalog
-from detectron2.utils import comm
-from detectron2.utils.file_io import PathManager
+from LabGym.detectron2.data import MetadataCatalog
+from LabGym.detectron2.utils import comm
+from LabGym.detectron2.utils.file_io import PathManager
 
 from .evaluator import DatasetEvaluator
 
@@ -178,7 +178,7 @@ def _print_panoptic_results(pq_res):
 
 
 if __name__ == "__main__":
-    from detectron2.utils.logger import setup_logger
+    from LabGym.detectron2.utils.logger import setup_logger
 
     logger = setup_logger()
     import argparse
