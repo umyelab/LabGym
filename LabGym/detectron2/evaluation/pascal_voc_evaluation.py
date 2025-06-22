a434eea392b5c553947d5032382418ac131d83e0
Yujia Hu <yujiahu415@gmail.com>
2025-06-21 23:36:41 -0400

    Update pascal_voc_evaluation.py

--- LabGym/detectron2/evaluation/pascal_voc_evaluation.py
+++ LabGym/detectron2/evaluation/pascal_voc_evaluation.py
@@ -10,9 +10,9 @@ from collections import OrderedDict, defaultdict
 from functools import lru_cache
 import torch
 
-from detectron2.data import MetadataCatalog
-from detectron2.utils import comm
-from detectron2.utils.file_io import PathManager
+from LabGym.detectron2.data import MetadataCatalog
+from LabGym.detectron2.utils import comm
+from LabGym.detectron2.utils.file_io import PathManager
 
 from .evaluator import DatasetEvaluator
 
