6cf6e8e104cf155fdcffb79ad429db130a12c13a
Yujia Hu <yujiahu415@gmail.com>
2025-06-21 23:36:07 -0400

    Update lvis_evaluation.py

--- LabGym/detectron2/evaluation/lvis_evaluation.py
+++ LabGym/detectron2/evaluation/lvis_evaluation.py
@@ -8,12 +8,12 @@ import pickle
 from collections import OrderedDict
 import torch
 
-import detectron2.utils.comm as comm
-from detectron2.config import CfgNode
-from detectron2.data import MetadataCatalog
-from detectron2.structures import Boxes, BoxMode, pairwise_iou
-from detectron2.utils.file_io import PathManager
-from detectron2.utils.logger import create_small_table
+import LabGym.detectron2.utils.comm as comm
+from LabGym.detectron2.config import CfgNode
+from LabGym.detectron2.data import MetadataCatalog
+from LabGym.detectron2.structures import Boxes, BoxMode, pairwise_iou
+from LabGym.detectron2.utils.file_io import PathManager
+from LabGym.detectron2.utils.logger import create_small_table
 
 from .coco_evaluation import instances_to_coco_json
 from .evaluator import DatasetEvaluator
