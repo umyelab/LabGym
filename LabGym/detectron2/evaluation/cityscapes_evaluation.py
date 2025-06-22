5040186f787af543dd29923e23a3c9ac2a707cc1
Yujia Hu <yujiahu415@gmail.com>
2025-06-21 23:34:46 -0400

    Update cityscapes_evaluation.py

--- LabGym/detectron2/evaluation/cityscapes_evaluation.py
+++ LabGym/detectron2/evaluation/cityscapes_evaluation.py
@@ -8,9 +8,9 @@ from collections import OrderedDict
 import torch
 from PIL import Image
 
-from detectron2.data import MetadataCatalog
-from detectron2.utils import comm
-from detectron2.utils.file_io import PathManager
+from LabGym.detectron2.data import MetadataCatalog
+from LabGym.detectron2.utils import comm
+from LabGym.detectron2.utils.file_io import PathManager
 
 from .evaluator import DatasetEvaluator
 
