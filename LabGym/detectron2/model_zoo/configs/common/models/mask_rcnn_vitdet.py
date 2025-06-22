c43f28fe27cfe4abe410144e60b7a268f6a45713
Yujia Hu <yujiahu415@gmail.com>
2025-06-21 23:49:15 -0400

    Update mask_rcnn_vitdet.py

--- LabGym/detectron2/model_zoo/configs/common/models/mask_rcnn_vitdet.py
+++ LabGym/detectron2/model_zoo/configs/common/models/mask_rcnn_vitdet.py
@@ -1,8 +1,8 @@
 from functools import partial
 import torch.nn as nn
-from detectron2.config import LazyCall as L
-from detectron2.modeling import ViT, SimpleFeaturePyramid
-from detectron2.modeling.backbone.fpn import LastLevelMaxPool
+from LabGym.detectron2.config import LazyCall as L
+from LabGym.detectron2.modeling import ViT, SimpleFeaturePyramid
+from LabGym.detectron2.modeling.backbone.fpn import LastLevelMaxPool
 
 from .mask_rcnn_fpn import model
 from ..data.constants import constants
