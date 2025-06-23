1fe4dfc839da520228d03d941c3d3390df3fe6da
Yujia Hu <yujiahu415@gmail.com>
2025-06-23 14:11:29 -0400

    Update video_visualizer.py

--- LabGym/detectron2/utils/video_visualizer.py
+++ LabGym/detectron2/utils/video_visualizer.py
@@ -3,8 +3,8 @@ import numpy as np
 from typing import List
 import pycocotools.mask as mask_util
 
-from detectron2.structures import Instances
-from detectron2.utils.visualizer import (
+from LabGym.detectron2.structures import Instances
+from LabGym.detectron2.utils.visualizer import (
     ColorMode,
     Visualizer,
     _create_text_labels,
