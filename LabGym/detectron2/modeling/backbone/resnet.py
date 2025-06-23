affa734186da1b46ac02f386a261784ba6b89041
Yujia Hu <yujiahu415@gmail.com>
2025-06-23 01:10:00 -0400

    Update resnet.py

--- LabGym/detectron2/modeling/backbone/resnet.py
+++ LabGym/detectron2/modeling/backbone/resnet.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from detectron2.layers import (
+from LabGym.detectron2.layers import (
     CNNBlockBase,
     Conv2d,
     DeformConv,
