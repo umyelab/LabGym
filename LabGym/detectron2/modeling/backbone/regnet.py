a7a4a656cc7eafa01384b32e7a676d1869bb216a
Yujia Hu <yujiahu415@gmail.com>
2025-06-23 01:09:43 -0400

    Update regnet.py

--- LabGym/detectron2/modeling/backbone/regnet.py
+++ LabGym/detectron2/modeling/backbone/regnet.py
@@ -10,7 +10,7 @@ model loading.
 import numpy as np
 from torch import nn
 
-from detectron2.layers import CNNBlockBase, ShapeSpec, get_norm
+from LabGym.detectron2.layers import CNNBlockBase, ShapeSpec, get_norm
 
 from .backbone import Backbone
 
