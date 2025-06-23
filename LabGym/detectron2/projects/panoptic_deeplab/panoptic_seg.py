3b1c722bb0ae7691cccfb1500e12f5c7c55330bf
Yujia Hu <yujiahu415@gmail.com>
2025-06-23 13:50:28 -0400

    Update panoptic_seg.py

--- LabGym/detectron2/projects/panoptic_deeplab/panoptic_seg.py
+++ LabGym/detectron2/projects/panoptic_deeplab/panoptic_seg.py
@@ -6,20 +6,20 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from detectron2.config import configurable
-from detectron2.data import MetadataCatalog
-from detectron2.layers import Conv2d, DepthwiseSeparableConv2d, ShapeSpec, get_norm
-from detectron2.modeling import (
+from LabGym.detectron2.config import configurable
+from LabGym.detectron2.data import MetadataCatalog
+from LabGym.detectron2.layers import Conv2d, DepthwiseSeparableConv2d, ShapeSpec, get_norm
+from LabGym.detectron2.modeling import (
     META_ARCH_REGISTRY,
     SEM_SEG_HEADS_REGISTRY,
     build_backbone,
     build_sem_seg_head,
 )
-from detectron2.modeling.postprocessing import sem_seg_postprocess
-from detectron2.projects.deeplab import DeepLabV3PlusHead
-from detectron2.projects.deeplab.loss import DeepLabCE
-from detectron2.structures import BitMasks, ImageList, Instances
-from detectron2.utils.registry import Registry
+from LabGym.detectron2.modeling.postprocessing import sem_seg_postprocess
+from LabGym.detectron2.projects.deeplab import DeepLabV3PlusHead
+from LabGym.detectron2.projects.deeplab.loss import DeepLabCE
+from LabGym.detectron2.structures import BitMasks, ImageList, Instances
+from LabGym.detectron2.utils.registry import Registry
 
 from .post_processing import get_panoptic_segmentation
 
