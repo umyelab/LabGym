1913ed8ca139fe2f572ed870ceb92153d0380251
Yujia Hu <yujiahu415@gmail.com>
2025-06-23 01:12:27 -0400

    Update retinanet.py

--- LabGym/detectron2/modeling/meta_arch/retinanet.py
+++ LabGym/detectron2/modeling/meta_arch/retinanet.py
@@ -7,10 +7,10 @@ from fvcore.nn import sigmoid_focal_loss_jit
 from torch import Tensor, nn
 from torch.nn import functional as F
 
-from detectron2.config import configurable
-from detectron2.layers import CycleBatchNormList, ShapeSpec, batched_nms, cat, get_norm
-from detectron2.structures import Boxes, ImageList, Instances, pairwise_iou
-from detectron2.utils.events import get_event_storage
+from LabGym.detectron2.config import configurable
+from LabGym.detectron2.layers import CycleBatchNormList, ShapeSpec, batched_nms, cat, get_norm
+from LabGym.detectron2.structures import Boxes, ImageList, Instances, pairwise_iou
+from LabGym.detectron2.utils.events import get_event_storage
 
 from ..anchor_generator import build_anchor_generator
 from ..backbone import Backbone, build_backbone
