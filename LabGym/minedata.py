978181c8019ee92cbfad16e0cec637e7adf72421
ruck94301 <John.Ruckstuhl@gmail.com>
2025-08-25 17:21:32 -0700

    scrubbed a trailing space
    This wasn't caught earlier because this file is CRLF instead of LF.

--- LabGym/minedata.py
+++ LabGym/minedata.py
@@ -2,7 +2,7 @@
 Copyright (C)
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program. If not, see https://tldrlegal.com/license/gnu-general-public-license-v3-(gpl-3)#fulltext. 
+You should have received a copy of the GNU General Public License along with this program. If not, see https://tldrlegal.com/license/gnu-general-public-license-v3-(gpl-3)#fulltext.
 For license issues, please contact:
 Dr. Bing Ye
 Life Sciences Institute
@@ -171,7 +171,7 @@ class data_mining():
 
 					else:
 
-						dunn=sp.posthoc_dunn(dataset) 
+						dunn=sp.posthoc_dunn(dataset)
 						dunn.columns=self.file_names
 						dunn.index=self.file_names
 						print('\t'*2,'Dunns post-hoc results:')
