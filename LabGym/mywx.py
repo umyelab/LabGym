6c0ad9d082f363ddfe53e181081e7512ee071737
ruck94301 <John.Ruckstuhl@gmail.com>
2025-10-28 10:45:59 -0700

    refactor bring_to_foreground to mywx module
    bring_to_foreground is implemented as a method in the registration dialog object.
    It's actually general, affecting the app, not specifically the dialog object.
    This commit moves it to a function in mywx, so it can be used even if registration dialog isn't employed.

--- LabGym/mywx.py
+++ LabGym/mywx.py
@@ -1,5 +1,12 @@
 """Provide function to guard from multiple instantiations of wx.App
 
+Public Functions
+    App -- Return the wx.App object.
+    bring_wxapp_to_foreground -- Bring the wx app to the foreground.
+
+Public Classes
+    None
+
 Use
     import mywx
     app = mywx.App()
@@ -9,7 +16,7 @@ or
 instead of
     import wx
     app = wx.App()
-to avoid creating two wx.App objects at once.  
+for added protection against creating two wx.App objects at once.  
 
 This implementation uses the common approach of using a module-level 
 variable to store the value after its initial creation.
@@ -21,6 +28,13 @@ collected when used like
     import mywx
     mywx.App()
 
+Note that mywx.App could be implemented as a subclass of wx.App instead 
+of a function.  It would use a custom __new__ method to guard against 
+creating multiple instances, and a custom __init__ method to guard 
+against re-initializing the singleton object.
+If a custom OnInit method was desired, the scales would be tipped in
+favor of the subclass implementation over the function implementation.
+
 "wx.App is supposed to be used as a singleton.  It doesn't own the 
 frames that are created in its OnInit, but instead assumes that it is 
 supposed to manage and deliver events to all the windows that exist in 
@@ -30,13 +44,25 @@ the application."
 toolkit has been initialized) not during the MainLoop call."
 """
 
+# Allow use of newer syntax Python 3.10 type hints in Python 3.9.
+from __future__ import annotations
+    
+# Standard library imports.
 import logging
+import sys
 
-import wx
+# Related third party imports.
+if sys.platform == 'darwin':  # macOS
+    # AppKit is from package pyobjc-framework-Cocoa, "Wrappers for the
+    # Cocoa frameworks on macOS".
+    from AppKit import NSApp, NSApplication
+import wx  # wxPython, Cross platform GUI toolkit for Python, "Phoenix" version
+
+# Local application/library specific imports.
+# (none)
 
 
 logger = logging.getLogger(__name__)
-
 
 _cached_app = None
 
@@ -57,3 +83,32 @@ def App():
 
     _cached_app = app
     return app
+
+
+def bring_wxapp_to_foreground() -> None:
+    """Bring the wx app to the foreground.
+
+    We want the wx app displayed to the user, not initially obscured by 
+    windows from other apps.
+
+    On macOS 12.7, with LabGym started from terminal, I'm seeing:
+    *   the registration dialog is displayed, but doesn't have focus
+        and is under the windows of the active app (terminology?),
+        potentially hidden completely.
+    *   a bouncing python rocketship icon in the tray.  The bouncing
+        stops when you mouseover the icon.  Also some other actions
+        can stop the bouncing or just pause the bouncing... weird.
+
+    I wasn't able to resolve this on macOS using the wx.Dialog object's 
+    Raise, SetFocus, and Restore methods.
+    Instead, this approach worked for me...
+        "Calling NSApp().activateIgnoringOtherApps_(True) via AppKit: 
+        This macOS workaround uses the AppKit module to explicitly 
+        activate the application, ignoring other running applications.  
+        This is typically done after your wxPython application has 
+        started and its main window is shown."
+    """ 
+
+    if sys.platform == 'darwin':  # macOS
+        NSApplication.sharedApplication()
+        NSApp().activateIgnoringOtherApps_(True)
