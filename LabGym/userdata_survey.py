79d3fe4126741381f5dc1774606eafca2d2886a8
ruck94301 <John.Ruckstuhl@gmail.com>
2025-10-28 18:55:31 -0700

    Implement offer_mkdir_userdata.
    Implement offer_mkdir_userdata function, and call it from userdata_survey.survey().

--- LabGym/userdata_survey.py
+++ LabGym/userdata_survey.py
@@ -57,9 +57,8 @@ def is_path_equivalent(path1: str|Path, path2: str|Path) -> bool:
     if Path(path1).exists() and Path(path2).exists():
         return Path(path1).samefile(Path(path2))
 
-    # if neither exists, are the strings the same
+    # if neither exists, are the strings of the resolved paths the same?
     if not Path(path1).exists() and not Path(path2).exists():
-        # return str(Path(path1).resolve()) == str(Path(path2).resolve())
         return resolve(path1) == resolve(path2)
 
     # At this point, one exists and the other doesn't, therefore False
@@ -70,6 +69,109 @@ def resolve(path1: str|Path) -> str:
     """Return a string representation of the resolved string or Path obj."""
     return str(Path(path1).resolve())
 
+
+def dict2str(arg: dict, hanging_indent: str=' '*16) -> str:
+    """Return a string representation of the dict, with a hanging indent.
+
+    Return '' if the dict is empty.
+
+    Why the hanging indent?  So that it can be tuned to avoid fouling 
+    the left-alignment when used inside a multiline string that will be 
+    dedented.
+    """
+
+    result = ('\n' + hanging_indent).join(
+        [f'{key}: {value}' for key, value in arg.items()]
+        )
+
+    return result
+
+
+def verify_userdata_dir_separation(
+        detectors_dir: str, models_dir: str) -> None:
+    """Verify the separation of configuration's userdata dirs.
+
+    If bad (not separate), then display an error message, then sys.exit().
+
+    Enforce an expectation that the detectors_dir and models_dir are
+    separate, and do not have a "direct, lineal relationshop", where one
+    is under the other.
+    """
+
+    if (is_path_equivalent(detectors_dir, models_dir)
+            or is_path_under(detectors_dir, models_dir)
+            or is_path_under(models_dir, detectors_dir)):
+
+        # fatal configuration error
+        title = 'LabGym Configuration Error'
+        msg = textwrap.dedent(f"""\
+            LabGym Configuration Error
+                detectors: {detectors_dir!r} ({resolve(detectors_dir)!r})
+                models: {models_dir!r} ({resolve(models_dir)!r})
+
+            The userdata dirs must be separate.
+            """)
+
+        logger.error('%s', msg)
+
+        # Show the error msg with an OK_Dialog.
+        with mywx.OK_Dialog(None, title=title, msg=msg) as dlg:
+            mywx.bring_wxapp_to_foreground()
+
+            result = dlg.ShowModal()  # will return wx.ID_OK upon OK or dismiss
+            logger.debug('%s: %r', 'result', result)
+
+            sys.exit('Bad configuration')
+    
+
+def offer_mkdir_userdata_dirs(
+        labgym_dir: str, detectors_dir: str, models_dir: str) -> None:
+    """Offer to attempt to mkdir userdata dirs if they are external.
+
+    If there are any userdata dirs that
+        (a) are defined in the LabGym configuration to be external to 
+            the LabGym sw directory,
+        (b) don't already exist. 
+    then offer to attempt to mkdir them.
+
+    Note that the attempts can fail and raise an exception for several
+    reasons, including insufficient permissions, or parent dir of the
+    target dir doesn't exist.
+    """
+
+    mkdir_targets = {}
+
+    if (not is_path_under(labgym_dir, detectors_dir) 
+            and not os.path.isdir(detectors_dir)):
+        mkdir_targets.update({'detectors': detectors_dir})
+    if (not is_path_under(labgym_dir, models_dir) 
+            and not os.path.isdir(models_dir)):
+        mkdir_targets.update({'models': models_dir})
+
+    if mkdir_targets:
+        title = 'LabGym Configuration: make dir(s)?'
+        msg = textwrap.dedent(f"""\
+            These folders are specified by your LabGym
+            configuration, but they don't exist yet.
+                {dict2str(mkdir_targets)}
+
+            Try to create them?
+            """)
+        logger.debug('%s:\n%s', 'msg', msg)
+
+        with mywx.OK_Cancel_Dialog(None, title=title, msg=msg) as dlg:
+            mywx.bring_wxapp_to_foreground()
+
+            result = dlg.ShowModal()  # will return wx.ID_OK upon OK or dismiss
+            logger.debug('%s: %r', 'result', result)
+
+            if result == wx.ID_OK:
+                 for value in mkdir_targets.values():
+                      # Deliberately using os.mkdir instead of os.makedirs.
+                      # to avoid unintentionally creating a mistake with
+                      # multiple levels...
+                      os.mkdir(value)
+        
 
 def survey(
     labgym_dir: str,
@@ -82,6 +184,9 @@ def survey(
         Verify the separation of configuration's userdata dirs.
         If bad (not separate), 
         then display an error message, then sys.exit().
+
+    2.  Check for user data dirs that are external, but don't exist.
+        If any, offer to attempt mkdir.
     """
 
     # Get all of the values needed from config.get_config().
@@ -100,6 +205,10 @@ def survey(
 
     # At this point, the configured detectors_dir and models_dir are not
     # in fundamental conflict, at least.
+
+    # 2.  Check for user data dirs that are external, but don't exist.
+    #     If any, offer to attempt mkdir.
+    offer_mkdir_userdata_dirs(labgym_dir, detectors_dir, models_dir)
 
     if enable_userdata_survey_exit:
         sys.exit(f'Exiting early.'
