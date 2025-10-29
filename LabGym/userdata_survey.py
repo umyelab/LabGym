0adcc92305b3d1589c28dc44d40b84db083a7879
ruck94301 <John.Ruckstuhl@gmail.com>
2025-10-29 08:02:45 -0700

    Partially implement advise_on_internal_userdata_dirs.

--- LabGym/userdata_survey.py
+++ LabGym/userdata_survey.py
@@ -3,8 +3,9 @@ Provide functions for surveying the locations of user data.
 
 Public functions:
     survey
-    verify_userdata_dir_separation
-    offer_mkdir_userdata_dirs
+    assert_userdata_dirs_are_separate
+    offer_to_mkdir_userdata_dirs
+    advise_on_internal_userdata_dirs
 
     is_path_under(path1: str|Path, path2: str|Path) -> bool
     is_path_equivalent(path1: str|Path, path2: str|Path) -> bool
@@ -14,14 +15,14 @@ Public functions:
 
 Public classes: None
 
-The survey function has an early-exit capability, for demonstration 
+The survey function has an early-exit capability, for demonstration
 purposes.  If LabGym is started with --enable userdata_survey_exit,
 then survey will call sys.exit('Intentionally exiting early').
 
 Design with paths as strings, or, paths as pathlib.Path objects?
 Since the paths are configured as strings, assume the calls from outside
 this module pass strings, and inside this module, developer is free to
-use pathlib where convenient.  
+use pathlib where convenient.
 In other words, for public functions, support string path args, and
 optionally, extend to support Path object args.
 """
@@ -78,8 +79,8 @@ def dict2str(arg: dict, hanging_indent: str=' '*16) -> str:
 
     Return '' if the dict is empty.
 
-    Why the hanging indent?  So that it can be tuned to avoid fouling 
-    the left-alignment when used inside a multiline string that will be 
+    Why the hanging indent?  So that it can be tuned to avoid fouling
+    the left-alignment when used inside a multiline string that will be
     dedented.
     """
 
@@ -90,7 +91,7 @@ def dict2str(arg: dict, hanging_indent: str=' '*16) -> str:
     return result
 
 
-def verify_userdata_dir_separation(
+def assert_userdata_dirs_are_separate(
         detectors_dir: str, models_dir: str) -> None:
     """Verify the separation of configuration's userdata dirs.
 
@@ -125,29 +126,29 @@ def verify_userdata_dir_separation(
             logger.debug('%s: %r', 'result', result)
 
             sys.exit('Bad configuration')
-    
 
-def offer_mkdir_userdata_dirs(
+
+def offer_to_mkdir_userdata_dirs(
         labgym_dir: str, detectors_dir: str, models_dir: str) -> None:
     """Offer to attempt to mkdir userdata dirs if they are external.
 
     If there are any userdata dirs that
-        (a) are defined in the LabGym configuration to be external to 
+        (a) are defined in the LabGym configuration to be external to
             the LabGym sw directory,
-        (b) don't already exist. 
+        (b) don't already exist.
     then offer to attempt to mkdir them.
 
-    Note that the attempts to mkdir can fail and raise an exception for 
-    several reasons, including insufficient permissions, or parent dir 
+    Note that the attempts to mkdir can fail and raise an exception for
+    several reasons, including insufficient permissions, or parent dir
     of the target dir doesn't exist.
     """
 
     mkdir_targets = {}
 
-    if (not is_path_under(labgym_dir, detectors_dir) 
+    if (not is_path_under(labgym_dir, detectors_dir)
             and not os.path.isdir(detectors_dir)):
         mkdir_targets.update({'detectors': detectors_dir})
-    if (not is_path_under(labgym_dir, models_dir) 
+    if (not is_path_under(labgym_dir, models_dir)
             and not os.path.isdir(models_dir)):
         mkdir_targets.update({'models': models_dir})
 
@@ -174,13 +175,51 @@ def offer_mkdir_userdata_dirs(
                       # to avoid unintentionally creating a mistake with
                       # multiple levels...
                       os.mkdir(value)
-        
 
-def check_for_internal_userdata_dirs(
+
+def advise_on_internal_userdata_dirs(
         labgym_dir: str, detectors_dir: str, models_dir: str) -> None:
     """
     """
-    pass
+    if not (is_path_under(labgym_dir, detectors_dir) or
+            is_path_under(labgym_dir, models_dir)):
+        # No advice is warranted.
+        return
+
+    # One or both userdata dirs is under labgym_dir.
+    # Prepare the message.
+
+    title = 'LabGym Configuration: internal userdata dirs'
+
+    msg = textwrap.dedent("""\
+        Your LabGym configuration (or the configuration 
+        defaults) presently define userdata dirs as 
+        located within the LabGym tree.
+        This arrangement is now deprecated.
+
+        Please follow these instructions to resolve,
+        then restart LabGym.
+        """)
+        #-----------------------------------------------
+
+    # if detectors_dir is internal, and no data is stored, then advise
+    # (a) configure new external detectors dir, (b) make the dir.
+    # elif detectors_dir is internal, and data is stored, then advise
+    # (a) configure new external detectors dir, (b) make the dir, 
+    # (d) back up data, (e) move data.
+
+    # msg += textwrap.dedent("""\
+    #     
+    #     """)
+    #     #-----------------------------------------------
+       
+    logger.debug('%s:\n%s', 'msg', msg)
+
+    with mywx.OK_Dialog(None, title=title, msg=msg) as dlg:
+        mywx.bring_wxapp_to_foreground()
+
+        result = dlg.ShowModal()  # will return wx.ID_OK upon OK or dismiss
+        logger.debug('%s: %r', 'result', result)
 
 
 def survey(
@@ -191,7 +230,7 @@ def survey(
     """Display guidance if userdata dirs are within the LabGym tree.
 
     1.  Verify the separation of configuration's userdata dirs.
-        If bad (not separate), 
+        If bad (not separate),
         then display an error message, then sys.exit().
 
     2.  Check for user data dirs that are external, but don't exist.
@@ -212,22 +251,22 @@ def survey(
     logger.debug('%s: %r', 'models_dir', models_dir)
 
     # 1.  Verify the separation of configuration's userdata dirs.
-    #     If bad (not separate), 
+    #     If bad (not separate),
     #     then display an error message, then sys.exit().
-    verify_userdata_dir_separation(detectors_dir, models_dir)
+    assert_userdata_dirs_are_separate(detectors_dir, models_dir)
 
-    # At this point, the configured detectors_dir and models_dir are not
-    # in fundamental conflict, at least.
+    # At this point, the configured detectors_dir models_dir are not in
+    # fundamental conflict, at least.
 
     # 2.  Check for user data dirs that are external, but don't exist.
     #     If any, offer to attempt mkdir.
-    offer_mkdir_userdata_dirs(labgym_dir, detectors_dir, models_dir)
+    offer_to_mkdir_userdata_dirs(labgym_dir, detectors_dir, models_dir)
 
     # 3.  Check for userdata dirs still located within the LabGym tree.
     #     If any, advise user to change config, make dirs, backup data,
     #     move data.  Press OK to quit LabGym.  Press Cancel to
     #     carry on with deprecated configuration.
-    check_for_internal_userdata_dirs(labgym_dir, detectors_dir, models_dir)
+    advise_on_internal_userdata_dirs(labgym_dir, detectors_dir, models_dir)
 
     if enable_userdata_survey_exit:
         sys.exit(f'Exiting early.'
